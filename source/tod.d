3af61db1acd831f37aad36aaaa5504b670721c66
Mina Kamel <minallkamel@gmail.com>
2022-07-12 06:08:07 +0200

    fix indentation

--- source/tod.d
+++ source/tod.d
@@ -6,52 +6,52 @@ import transpiler;
 
 void main()
 {
-	auto checkArgs = new Program();
-	checkArgs
-	.ifInit()		  
-	
-	.ifConditionAdd("args.length > 1")
-	
-	.ifEnd()
-	
-	.blockInit()
-	.functionCallInit("writeln")
-	.argsInit()
-	.argsAdd("args[1]")
-	.argsEnd()
-	.functionCallEnd()
-	.blockEnd()
-	
-	.elseInit()
+    auto checkArgs = new Program();
+    checkArgs
+    .ifInit()		  
+    
+    .ifConditionAdd("args.length > 1")
+    
+    .ifEnd()
+    
+    .blockInit()
+    .functionCallInit("writeln")
+    .argsInit()
+    .argsAdd("args[1]")
+    .argsEnd()
+    .functionCallEnd()
+    .blockEnd()
+    
+    .elseInit()
 
-	.blockInit()
-	.functionCallInit("writeln")
-	.argsInit()
-	.argsAdd("args[0]")
-	.argsEnd()
-	.functionCallEnd()
-	.blockEnd()
+    .blockInit()
+    .functionCallInit("writeln")
+    .argsInit()
+    .argsAdd("args[0]")
+    .argsEnd()
+    .functionCallEnd()
+    .blockEnd()
 
-	.elseEnd();
-	
-	////////////////////////////////////////////////////////////////////
-	
-	auto mainProgram = new Program();
-	mainProgram
-	.importStd("stdio")
-	.functionInit("int", "main")
-	.paramsInit()
-	.paramsAdd("string[]", "args")
-	.paramsEnd()
-	.blockInit()
-	.join(checkArgs)
-	.functionReturn("0")
-	.blockEnd()
-	.functionEnd();
+    .elseEnd();
+    
+    ////////////////////////////////////////////////////////////////////
+    
+    auto mainProgram = new Program();
+    mainProgram
+    .importStd("stdio")
+    .functionInit("int", "main")
+    .paramsInit()
+    .paramsAdd("string[]", "args")
+    .paramsEnd()
+    .blockInit()
+    .join(checkArgs)
+    .functionReturn("0")
+    .blockEnd()
+    .functionEnd();
 
-	/////////////////////////////////////////////////////////////////
-	auto output = "test";
-	auto t = new Transpiler(output, mainProgram);
-	
-	t.write("examples");
+    /////////////////////////////////////////////////////////////////
+    auto output = "test";
+    auto t = new Transpiler(output, mainProgram);
+    
+    t.write("examples");
 }
